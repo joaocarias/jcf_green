67f5dee73ef4f180df4bb3a0ef01eff7b578a67d
João Carias de França <joaocariasdefranca@gmail.com>
2023-02-15 08:50:52 -0300

    criando subprojetos base

---
+++ Jcf.Green/src/Jcf.Green.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Jcf.Green.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
